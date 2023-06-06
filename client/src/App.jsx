0d25967033a2fc192f79fabad8e6c23cc5d2093c
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-06 21:55:05 +0545

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <>
-    Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
