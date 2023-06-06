b715ce8b62575da7df08697ab0f16334d34e8cb3
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-06 21:33:24 +0545

    connect: mongoose

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = mongoose.connect(process.env.MONGO_URI);
+        if (conn) {
+            console.log('MongoDB connected');
+        }else{
+            console.log('MongoDB not connected');
+        }
+
+        // await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
