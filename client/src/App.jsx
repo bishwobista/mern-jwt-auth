d83846fb334e7a51347b7f11b9bbfb0853bc6d9d
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-07 08:20:53 +0545

    add : react-hook-form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import Login from "./pages/Login";
+
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<SignIn/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
         </Routes>
