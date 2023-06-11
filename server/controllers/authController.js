82abc2c19f16624ed66e2321961907c1ffaaf691
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-11 22:01:46 +0545

    added login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,4 +18,18 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try{
+    const user = await User.findOne({ email, password });
+    if (user) {
+      return res.status(200).send({success:true, message: "User logged in" });
+    }else{
+      return res.send({success:false, message: "User not logged in" });
+    }
+  }catch(error){
+    return res.status(400).send({success:false, message: "User not logged in" });
+  }
+}
+
+module.exports = { registerUser, loginUser };
