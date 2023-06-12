137f1f1d23481f8f33af4b5d09bc04d3f9b1ad2d
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-12 12:01:47 +0545

    use cors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const port = 3000;
 
+app.use(cors());
 const authRoute = require('./routes/authRoute');
 
 app.get('/', (req, res) => {
@@ -16,12 +17,9 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+
 app.use('/auth', authRoute);
-// app.post('/auth/register', (req, res) => {
-//     console.log(req.body);
-//     res.send('Hello World!');
-// })
+
 
 app.listen(port, () => {
     console.log(`it's live at http://localhost:${port}`);
