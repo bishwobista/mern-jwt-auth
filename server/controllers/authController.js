b22522211245aa500f18eb0b3873fa4bbc555ce5
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-12 11:58:28 +0545

    fixed typo

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,20 +1,19 @@
 const User = require('../models/UserModel');
 
 const registerUser = async (req, res) => {
-  const { user, email, password } = req.body;
+  const { name, email, password } = req.body;
   const userExists = await User.findOne({ email });
   if (userExists) {
     return res.status(200).send({success:false, message: "User already exists" });
   }else{
-    try{
-       const newEntry = new User(req.body);
-       newEntry.save();
-       console.log(newEntry);
-       return res.status(200).send({success:true, message: "User created" });
-    }
-    catch(err){
-         return res.status(400).send({success:false, message: "User not created" });
-    }
+    
+      const newUser = new User({ name, email, password });
+      await newUser.save();
+      return res.status(200).send({success:true, message: "User registered" });
+
+       
+      
+    
   }
 };
 
