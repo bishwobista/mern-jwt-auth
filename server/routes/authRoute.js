f4b6c0a1ee4750dad4457d25808947f8bab23a84
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-12 12:01:07 +0545

    add : /register route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {registerUser, loginUser} = require('../controllers/authController');
-router.post('/register',(req,res)=>{
-    registerUser;
-});
-// router.post('/register', registerUser);
+router.post('/register', registerUser);
 router.post('/login', loginUser);
 module.exports = router;
