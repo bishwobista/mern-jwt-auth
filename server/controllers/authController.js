22f2a2067733c5a38df603b2ed4d866f586e88ef
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-15 09:50:16 +0545

    add password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,34 +1,41 @@
-const User = require('../models/UserModel');
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   const userExists = await User.findOne({ email });
   if (userExists) {
-    return res.status(200).send({success:false, message: "User already exists" });
-  }else{
-    
-      const newUser = new User({ name, email, password });
-      await newUser.save();
-      return res.status(200).send({success:true, message: "User registered" });
-
-       
-      
-    
+    return res
+      .status(200)
+      .send({ success: false, message: "User already exists" });
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: hash,
+    });
+    // const newUser = new User({ name, email, password });
+    // await newUser.save();
+    return res.status(200).send({ success: true, message: "User registered" });
   }
 };
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  try{
-    const user = await User.findOne({ email, password });
-    if (user) {
-      return res.status(200).send({success:true, message: "User logged in" });
-    }else{
-      return res.send({success:false, message: "User not logged in" });
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) { 
+      return res.status(200).send({ success: true, message: "User logged in" });
+    } else {
+      return res.send({ success: false, message: "User not logged in" });
     }
-  }catch(error){
-    return res.status(400).send({success:false, message: "User not logged in" });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ success: false, message: "User not logged in" });
   }
-}
+};
 
 module.exports = { registerUser, loginUser };
