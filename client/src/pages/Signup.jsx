3a539dd32e978cacdfe9f835172da7968a54c625
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-16 15:06:48 +0545

    add password validation

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -11,8 +11,8 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { DevTool } from "@hookform/devtools";
 import api from "../config/api";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function SignUp() {
   const {
@@ -30,7 +30,8 @@ export default function SignUp() {
         password: data.password,
       };
       console.log(userData);
-      await api.post("/auth/register", userData)
+      await api
+        .post("/auth/register", userData)
         .then((log) => {
           if (log.data.success) {
             // alert("User Registered Successfully");
@@ -43,8 +44,7 @@ export default function SignUp() {
               draggable: true,
               progress: undefined,
               theme: "light",
-              });
-
+            });
           } else {
             // alert("User Registration Failed");
             toast.error(log.data.message, {
@@ -56,7 +56,7 @@ export default function SignUp() {
               draggable: true,
               progress: undefined,
               theme: "light",
-              });
+            });
           }
         })
         .catch((err) => {
@@ -73,160 +73,167 @@ export default function SignUp() {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
     }
   };
   return (
     <>
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 7,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
+      <Container component="main" maxWidth="xs">
         <Box
-          component="form"
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
-          sx={{ mt: 3 }}
+          sx={{
+            marginTop: 7,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Controller
-                name="fullName"
-                control={control}
-                defaultValue=""
-                rules={{
-                  required: "Full Name is required",
-                }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    autoComplete="full-name"
-                    fullWidth
-                    id="fullName"
-                    label="Full Name"
-                    autoFocus
-                    error={!!errors.fullName}
-                    helperText={
-                      errors.fullName ? errors.fullName.message : null
-                    }
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                rules={{
-                  required: "Email Address is required",
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: "Invalid email address",
-                  },
-                }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    autoComplete="email"
-                    error={!!errors.email}
-                    helperText={errors.email ? errors.email.message : null}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                rules={{
-                  required: "Password is required",
-                }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                    error={!!errors.password}
-                    helperText={
-                      errors.password ? errors.password.message : null
-                    }
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Controller
-                name="confirmPassword"
-                control={control}
-                defaultValue=""
-                rules={{
-                  required: "Confirm Password is required",
-                  validate: (value) =>
-                    value === getValues("password") || "Passwords do not match",
-                }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    required
-                    fullWidth
-                    name="confirmPassword"
-                    label="Confirm Password"
-                    type="password"
-                    id="confirm-password"
-                    autoComplete="confirm-password"
-                    error={!!errors.confirmPassword}
-                    helperText={
-                      errors.confirmPassword
-                        ? errors.confirmPassword.message
-                        : null
-                    }
-                  />
-                )}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ mt: 3 }}
           >
-            Sign Up
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="/" variant="body2">
-                Already have an account? Sign in
-              </Link>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Controller
+                  name="fullName"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "Full Name is required",
+                  }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      autoComplete="full-name"
+                      fullWidth
+                      id="fullName"
+                      label="Full Name"
+                      autoFocus
+                      error={!!errors.fullName}
+                      helperText={
+                        errors.fullName ? errors.fullName.message : null
+                      }
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "Email Address is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "Invalid email address",
+                    },
+                  }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      autoComplete="email"
+                      error={!!errors.email}
+                      helperText={errors.email ? errors.email.message : null}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "Password is required",
+                    pattern: {
+                      value:
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                      message:
+                        "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one special character",
+                    },
+                  }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                      error={!!errors.password}
+                      helperText={
+                        errors.password ? errors.password.message : null
+                      }
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Controller
+                  name="confirmPassword"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "Confirm Password is required",
+                    validate: (value) =>
+                      value === getValues("password") ||
+                      "Passwords do not match",
+                  }}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      required
+                      fullWidth
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      type="password"
+                      id="confirm-password"
+                      autoComplete="confirm-password"
+                      error={!!errors.confirmPassword}
+                      helperText={
+                        errors.confirmPassword
+                          ? errors.confirmPassword.message
+                          : null
+                      }
+                    />
+                  )}
+                />
+              </Grid>
             </Grid>
-          </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+          <DevTool control={control} />
         </Box>
-        <DevTool control={control} />
-      </Box>
-    </Container>
+      </Container>
       <ToastContainer />
-      </>
+    </>
   );
 }
