9a3e7c163f77fcd0be439b525511f721cd928751
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-18 17:56:05 +0545

    add : protected and public routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link , useNavigate} from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import { useEffect } from "react";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignIn/>} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<PublicRoute><SignIn/></PublicRoute>  } />
+          <Route path="/signup" element= { <PublicRoute><SignUp /></PublicRoute> } />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          {/* <Route path="/" element={<SignIn/>  } />
+          <Route path="/signup" element= { <SignUp /> } />
+          <Route path="/home" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
     </>
@@ -19,3 +23,29 @@ function App() {
 
 export default App;
 
+export function ProtectedRoute({ children }) {
+  
+  const data = localStorage.getItem("data");
+  const navigate = useNavigate();
+  if (data) {
+    return children;
+  } else {
+    useEffect(() => {
+      navigate("/");
+    }
+    )
+  }
+}
+export function PublicRoute({ children }) {
+  const data = localStorage.getItem("data");
+  const navigate = useNavigate();
+  if (!data) {
+    return children;
+  } else {
+    useEffect(() => {
+    navigate("/home");
+    }
+    )
+    return null;
+  }
+}
