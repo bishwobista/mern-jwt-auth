c4c9ed7858a644ce42ccbf49c40f4e06ce1374e1
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-18 20:41:43 +0545

    decrypting jwt

---
+++ server/config/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const user = jwt.verify(token, "secret key");
+            req.body.user = user;
+            next();
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({success: false, error: error});
+        }
+    }else{
+        res.send({success: false, message: "Not authorized"});
+    }
+    if(!token){
+        return res.status(401).send({success: false, message: "Not authorized"});
+    }else{
+        res.send({success: true, message: "Authorized"});
+    }
+    
+}
