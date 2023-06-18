30dce1475f21ab360a80eaea81fd22b4fa5cb07e
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-18 20:42:51 +0545

    add: userdata route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser} = require('../controllers/authController');
+const {registerUser, loginUser, userData} = require('../controllers/authController');
+const authMiddleware = require('../config/authMiddleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/userdata', authMiddleware, userData);
+// router.post('/userdata', (req, res) => res.send({success: true, message: "Authorized"}));
 module.exports = router;
