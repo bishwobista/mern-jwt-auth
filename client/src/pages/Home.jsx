42168ee01b07b02bb7ff85ff776857e92fd12c0f
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-18 17:54:47 +0545

    add landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import {
+  Avatar,
+  Box,
+  Container,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Input, InputLabel, FormHelperText } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import React from "react";
 
 const Home = () => {
+  const updateSubmit = () => {};
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Container maxWidth="lg">
+        <Box sx={{ my: 4 }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <AccountCircleIcon />
+          </Avatar>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Welcome User
+          </Typography>
+          <FormControl>
+            <Grid container spacing={2} my={1}>
+              <Grid item xs={12} sm={6}>
+                <InputLabel htmlFor="my-email">Email address</InputLabel>
+                <Input id="my-email" aria-describedby="my-email-helper-text" />
+                <FormHelperText id="my-email-helper-text">
+                  update your Email
+                </FormHelperText>
+              </Grid>
+            </Grid>
+          </FormControl>
+        </Box>
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
