952b4067f88329aa48fd746114fe8f0cd8d8f916
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-19 21:33:14 +0545

    add update route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, userData} = require('../controllers/authController');
+const {registerUser, loginUser, userData, updateUser} = require('../controllers/authController');
 const authMiddleware = require('../config/authMiddleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/userdata', authMiddleware, userData);
-// router.post('/userdata', (req, res) => res.send({success: true, message: "Authorized"}));
+router.post('/update',updateUser)
 module.exports = router;
