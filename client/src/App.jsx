8272395dd7cb37811fe0fce5d1dc45219b6b3eca
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-22 10:15:19 +0545

    add : verify email page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
+import VerifyEmail from "./pages/VerifyEmail";
 import { BrowserRouter, Routes, Route, Link , useNavigate} from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import { useEffect } from "react";
@@ -9,12 +10,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PublicRoute><SignIn/></PublicRoute>  } />
-          <Route path="/signup" element= { <PublicRoute><SignUp /></PublicRoute> } />
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          {/* <Route path="/" element={<SignIn/>  } />
-          <Route path="/signup" element= { <SignUp /> } />
-          <Route path="/home" element={<Home />} /> */}
+          <Route path= '/'element={<PublicRoute><SignIn/></PublicRoute>  } />
+          <Route path='/signup'element= { <PublicRoute><SignUp /></PublicRoute> } />
+          <Route path='/home'element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/verify/:token" element={<PublicRoute><VerifyEmail /></PublicRoute>} />
+          {/* <Route path='/'element={<SignIn/>  } />
+          <Route path='/signup'element= { <SignUp /> } />
+          <Route path='/home'element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
     </>
