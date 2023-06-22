fb12b272f047a93817723b0a021d5d229916717e
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-22 09:25:12 +0545

    add verify mail route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, userData, updateUser} = require('../controllers/authController');
+const {registerUser, loginUser, userData, updateUser, verifyMail} = require('../controllers/authController');
 const authMiddleware = require('../config/authMiddleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/userdata', authMiddleware, userData);
-router.post('/update',updateUser)
+router.post('/update',updateUser);
+router.post('/verify-mail', verifyMail)
 module.exports = router;
